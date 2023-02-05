6bb35f289aabd4d7dc16eec75d3a62af9e76a136
Nadeemraffe <123829445+Nadeemraffe@users.noreply.github.com>
2023-02-05 08:50:59 -0800

    Add files via upload

---
+++ CHAP5/app.js
@@ -0,0 +1,139 @@
+// Qno1
+// var num1=1;
+// var num2=2;
+// num4=num1+num2;
+// document.write(num4);
+// Qno1
+// var num1=2;
+// var num2=2;
+// num4=num1*num2;
+// document.write(num4);
+// Qno1
+// var num1=6;
+// var num2=2;
+// num4=num1/num2;
+// document.write(num4);
+// Qno1
+// var num1=1;
+// var num2=2;
+// num4=num1 - num2;
+// document.write(num4);
+// Qno2
+// var number;
+// document.write("<p>Value after variable declaration is undefined</p>");
+// number = 5;
+// document.write("<p>Initial value: 5</p>");
+// number= ++number;
+// document.write("<p>Value after increment is 6</p>");
+// number= number + 7;
+// document.write("<p>Value after addition is 13</p>");
+// number= --number;
+// document.write("<p>Value after decrement is 12</p>");
+// number= number / 3;
+// document.write("<p>The remainder is 4</p>");
+// var number;
+// document.write("<p>Value after variable declaration is undefined</p>");
+// number = 5;
+// document.write("<p>Initial value: 5</p>");
+// number= ++number;
+// document.write("<p>Value after increment is 6</p>");
+// number= number + 7;
+// document.write("<p>Value after addition is 13</p>");
+// number= --number;
+// document.write("<p>Value after decrement is 12</p>");
+// number= number / 3;
+// document.write("<p>The remainder is 4</p>");
+// Question 4
+
+// var ticketcost= 600;
+// totalcost= ticketcost * 5;
+// document.write(totalcost);
+
+//Question 5
+
+// document.write("<h3>Table of 5</h3>");
+// document.write("<p>5x1=5</p>");
+// document.write("<p>5x2=10</p>");
+// document.write("<p>5x3=15</p>");
+// document.write("<p>5x4=20</p>");
+// document.write("<p>5x5=25</p>");
+// document.write("<p>5x6=30</p>");
+// document.write("<p>5x7=35</p>");
+// document.write("<p>5x8=40</p>");
+// document.write("<p>5x9=45</p>");
+// document.write("<p>5x10=50</p>");
+
+//Question 6
+
+//var celsius = 26;
+//celsius = (celsius * 9/5)+32;
+//document.write("26<sup>0</sup>C is 78.8<sup>0</sup>F");
+//var farenheit= 79;
+//farenheit = (farenheit - 32)*5/9;
+//document.write("79<sup>0</sup>F is 26.1<sup>0</sup>C");
+
+//Question 7
+
+// var priceOfItem1 = 600;
+// var priceOfitem2 = 400;
+// var quantityOfItem1 = 5;
+// var quantityOfItem2 = 10;
+// var shippingCharges = 200;
+// var totalcost = (priceOfItem1 * quantityOfItem1)+(priceOfitem2 * quantityOfItem2)+shippingCharges;
+// document.write("<h3>Shopping Cart</h3>");
+// document.write("<p>Price of Item 1 is 600</p>");
+// document.write("<p>Quantity of item 1 is 5</p>");
+// document.write("<p>Price of Item 2 is 400</p>");
+// document.write("<p>Quantity of item 2 is 10</p>");
+// document.write("<p>Shipping charges are 200</p>");
+// document.write("<p>Total cost of your order is 7200</p>");
+
+//Question 8
+
+// var totalMarks = 850;
+// var marksObtained = 700;
+// var percentage = 700 / 850 * 100;
+// document.write(percentage);
+
+//Question 9
+
+// var dollar = 10;
+// var dollarrate = 104.80;
+// var riyals = 25;
+// var riyalrate = 28;
+// var PKR = (dollar * dollarrate) + (riyals * riyalrate);
+// document.write("<h3>Currency in PKR</h3>");
+// document.write(PKR);
+
+//Question 10
+
+// var num = 10;
+// var num = (num + 5) * 10 /2;
+// document.write(num);
+
+//Question 11
+
+// var currentYear = 2023;
+// var birthYear = 2003;
+// var age = currentYear - birthYear;
+// document.write(age);
+// document.write("<h3>Age Calculator</h3>");
+// document.write("Your age is either 19 or 20");
+
+//Question 12
+
+// var radius = 20;
+// var circumference = 2 * 3.142 * 20;
+// document.write("<h3>The Geometrizer</h3>");
+// document.write("<p>The Circumference is  125.6799</p>");
+// var area = 3.142 * 400;
+// document.write("The area is 1256.8");
+
+//Question 13
+
+// var favsnack = "Lays";
+// var age = 19;
+// var maxage = 60;
+// var maxamount = 3;
+// var lifetimesupply = maxamount * 41 * 12;
+// document.write("You will need 1476 lays to last you until the ripe old age of 60");
